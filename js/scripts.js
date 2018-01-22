ba41fb45d62b0b9d8c1ade7a884b7c2b065434f1
Mikhail Petrov and Maggie Summers <maggie@>
2018-01-22 10:16:14 -0800

    implement different js logic, add html form to interact with it

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,79 @@
 // Biz logic
-function Destination(location, timeOfYear, notes) {
+function Place(location, timeOfYear, advice) {
   this.location = location;
   this.timeOfYear = timeOfYear;
-  this.notes = notes;
+  this.advice = advice;
 }
 
-var destinationOne = new Destination("London", "summer", "go sightseeing");
-var destinationTwo = new Destination("Iceland", "spring", "go on a road trip around the country");
-var destinationThree = new Destination("Chile", "winter", "drink wine");
+//ui logic
 
-// ui logic
 $(document).ready(function() {
-
-  $("ul#destinations h3").append("<li><span class='info1'>" + destinationOne.location + "</span></li>");
-  $("ul#destinations h3").append("<li><span class='info2'>" + destinationTwo.location + "</span></li>");
-  $("ul#destinations h3").append("<li><span class='info3'>" + destinationThree.location + "</span></li>");
-
-  $(".info1").click(function(event) {
+  $("form#new-input").submit(function(event) {
     event.preventDefault();
-    $("#places_info").toggle();
-    $(".location").text(destinationOne.location);
-    $(".time_of_year").text(destinationOne.timeOfYear);
-    $(".notes").text(destinationOne.notes);
-  });
 
-  $(".info2").click(function(event) {
-    event.preventDefault();
-    $("#places_info").toggle();
-    $(".location").text(destinationTwo.location);
-    $(".time_of_year").text(destinationTwo.timeOfYear);
-    $(".notes").text(destinationTwo.notes);
-  });
+    var inputtedLocation = $("input#place").val();
+    var inputtedTime = $("input#time").val();
+    var inputtedAdvice = $("input#advice").val();
 
-  $(".info3").click(function(event) {
-    event.preventDefault();
-    $("#places_info").toggle();
-    $(".location").text(destinationThree.location);
-    $(".time_of_year").text(destinationThree.timeOfYear);
-    $(".notes").text(destinationThree.notes);
-  });
+    var newPlace = new Place(inputtedLocation, inputtedTime, inputtedAdvice);
 
+    $("#places").show();
+    $("ul#places").append("<li><span class='place'>" + newPlace.location + "</span></li>");
+
+    $(".place").last().click(function() {
+      $("#hidden").show();
+      $(".time").text(newPlace.timeOfYear);
+      $(".advice").text(newPlace.advice);
+    });
+
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var destinationOne = new Destination("London", "summer", "go sightseeing");
+// var destinationTwo = new Destination("Iceland", "spring", "go on a road trip around the country");
+// var destinationThree = new Destination("Chile", "winter", "drink wine");
+//
+// // ui logic
+// $(document).ready(function() {
+//
+//   $("ul#destinations h3").append("<li><span class='info1'>" + destinationOne.location + "</span></li>");
+//   $("ul#destinations h3").append("<li><span class='info2'>" + destinationTwo.location + "</span></li>");
+//   $("ul#destinations h3").append("<li><span class='info3'>" + destinationThree.location + "</span></li>");
+//
+//   $(".info1").click(function(event) {
+//     event.preventDefault();
+//     $("#places_info").toggle();
+//     $(".location").text(destinationOne.location);
+//     $(".time_of_year").text(destinationOne.timeOfYear);
+//     $(".notes").text(destinationOne.notes);
+//   });
+//
+//   $(".info2").click(function(event) {
+//     event.preventDefault();
+//     $("#places_info").toggle();
+//     $(".location").text(destinationTwo.location);
+//     $(".time_of_year").text(destinationTwo.timeOfYear);
+//     $(".notes").text(destinationTwo.notes);
+//   });
+//
+//   $(".info3").click(function(event) {
+//     event.preventDefault();
+//     $("#places_info").toggle();
+//     $(".location").text(destinationThree.location);
+//     $(".time_of_year").text(destinationThree.timeOfYear);
+//     $(".notes").text(destinationThree.notes);
+//   });
+//
+// });
